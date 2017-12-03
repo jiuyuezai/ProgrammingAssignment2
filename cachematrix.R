898630d0d11154251ce98db2cc6166616d8c71f2
jiuyuezai <hellosylviawong@gmail.com>
2017-12-03 10:36:31 +0800

    fix a bug

--- cachematrix.R
+++ cachematrix.R
@@ -1,4 +1,4 @@
-m## Caching the Inverse of a Matrix:
+## Caching the Inverse of a Matrix:
 ## Matrix inversion is usually a costly computation,
 ## there may be some benefit to caching the inverse 
 ## of a matrix rather than compute it repeatedly.
